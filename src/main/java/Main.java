9cbda482c95e92aca113cc0824e65f6639621423
dounia <dounia.meroug01@gmail.com>
2023-11-12 09:09:03 +0100

    update main 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("addition multiplication "+calculatorService.multiplication(7.0,2.0));
         System.out.println("addition division "+calculatorService.division(7.0,8.0));
         System.out.println("is prime "+calculatorService.isPrime(23));
+        System.out.println("is prime "+calculatorService.isPrime(11));
 
     }
 }
