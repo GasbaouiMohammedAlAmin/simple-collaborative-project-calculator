54119b2cb2eea2d6276f11b97693a45ff65e4e71
sanamech <sanamechraoui49@gmail.com>
2023-11-12 09:07:34 -0800

    update main3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("subtraction operation "+calculatorService.subtraction(5.0,6.0));
         System.out.println("addition multiplication "+calculatorService.multiplication(7.0,2.0));
         System.out.println("addition division "+calculatorService.division(7.0,8.0));
-        System.out.println("is prime "+calculatorService.isPrime(23));
+        System.out.println("is prime "+calculatorService.isPrime(31));
 
     }
 }
